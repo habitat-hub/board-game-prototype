583565ac26a49adf0b65f8a2168974a71bef019f
Keisuke Nemura <nemuraystk@gmail.com>
2024-11-24 22:27:02 +0900

    add first commit

---
+++ client/.eslintrc.js
@@ -0,0 +1,23 @@
+export default {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "prettier/prettier": "error",
+    // 他のカスタムルールをここに追加
+  },
+};
