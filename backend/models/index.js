0baa3e51b91637b2502b2c20cadd0eb9143c3f1d
Keisuke Nemura <nemuraystk@gmail.com>
2024-12-01 00:32:48 +0900

    fix 環境変数を読み取るように修正

--- backend/models/index.js
+++ backend/models/index.js
@@ -6,6 +6,8 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const db = {};
 
+require('dotenv').config();
+
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 fs.readdirSync(__dirname)
