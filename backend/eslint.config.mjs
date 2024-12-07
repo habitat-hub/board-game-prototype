4747a713d8a51fcf00d6717a54223053c2a1ae78
Keisuke Nemura <nemuraystk@gmail.com>
2024-12-07 22:32:10 +0900

    refactor: replace ESLint configuration with new format and add TypeScript support
    - Removed the old ESLint configuration file (.eslintrc.js).
    - Added a new ESLint configuration file (eslint.config.mjs) that supports TypeScript and includes recommended settings.
    - Updated package.json to include a lint script and added typescript-eslint as a dependency.
    - Updated package-lock.json to reflect changes in dependencies and versions.

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from 'globals';
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
+
+export default [
+  eslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+      globals: {
+        ...globals.commonjs,
+        ...globals.es2021,
+        ...globals.node,
+      },
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  {
+    ignores: ['dist/**/*', 'src/generate-erd.ts'],
+  },
+];
