511249bfd6bd2a6d36c6bd635b01de80134b4057
tom-takeru <tom.2461.ty@gmail.com>
2025-06-05 08:48:28 +0900

    feat: add metadata for the Home component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,12 @@
+import { Metadata } from 'next';
 import React from 'react';
 
 import About from '@/features/about/components/templates/About';
+
+export const metadata: Metadata = {
+  title: 'KIBAKOについて',
+  description: 'ボードゲーム試作品作成アプリ「KIBAKO」の紹介ページです。',
+};
 
 function Home() {
   return <About />;
