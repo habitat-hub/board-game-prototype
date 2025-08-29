3880aed76af0a6084831ab00a112ff2de1422af3
tom-takeru <tom.2461.ty@gmail.com>
2025-08-29 10:21:04 +0900

    backend: fix format error

--- backend/src/constants/file.ts
+++ backend/src/constants/file.ts
@@ -5,4 +5,7 @@ export const FILE_DEFAULT_NAME = 'untitled';
 // 画像特有の定数
 export const IMAGE_MAX_SIZE_MB = 10; // 10MB
 export const IMAGE_MAX_SIZE = IMAGE_MAX_SIZE_MB * 1024 * 1024; // In bytes
-export const IMAGE_ALLOWED_MIME_TYPES: ReadonlyArray<string> = ['image/jpeg', 'image/png'];
+export const IMAGE_ALLOWED_MIME_TYPES: ReadonlyArray<string> = [
+  'image/jpeg',
+  'image/png',
+];
