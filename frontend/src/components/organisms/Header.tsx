b3b380b6681e8f7068608ad5ba5e6e7a4aef3c38
tom-takeru <57748294+tom-takeru@users.noreply.github.com>
2025-09-01 08:19:37 +0900

    fix: redirect header logo to top page

--- frontend/src/components/organisms/Header.tsx
+++ frontend/src/components/organisms/Header.tsx
@@ -10,19 +10,18 @@ const Header: React.FC = () => {
   const pathname = usePathname();
 
   /**
-   * プロジェクト一覧画面へ遷移する
-   * 現在すでにプロジェクト一覧画面またはログイン画面にいる場合は何もしない
+   * トップページへ遷移する
+   * 現在すでにトップページにいる場合は何もしない
    */
-  const goToProjects = () => {
-    // プロジェクト一覧画面、またはログイン画面の場合
-    if (pathname === '/projects' || pathname === '/') return;
+  const goToTop = () => {
+    if (pathname === '/') return;
 
-    router.push('/projects');
+    router.push('/');
   };
 
   return (
     <header className="sticky top-0 z-40 bg-kibako-primary/80 px-8 flex justify-between items-center h-20 backdrop-blur-sm">
-      <button onClick={goToProjects}>
+      <button onClick={goToTop}>
         <div className="flex gap-2">
           <GiWoodenCrate className="text-4xl drop-shadow-xl transform -rotate-6 text-kibako-white" />
           <span className="text-3xl tracking-wider font-medium text-kibako-white">
